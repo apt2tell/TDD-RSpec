6665ef7e25d1f715bd866d51e954319024eb2dc4
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-05 14:18:44 +0100

    Pass factorial method  test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../solver'
 
-describe Solver
+describe Solver do
   before do
     @solver = Solver.new
   end
@@ -8,6 +8,7 @@ describe Solver
   describe '#factorial' do
     it 'should return the fatorial of N' do
       expect(@solver.factorial(0)).to eq 1
+      expect(@solver.factorial(1)).to eq 1
     end
   end
 end
