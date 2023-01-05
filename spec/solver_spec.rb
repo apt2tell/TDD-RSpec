106c77ab0242e13aff40a33b12a34e8c4347baa0
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-05 14:22:32 +0100

    Test case for factorial n = 6

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -10,5 +10,9 @@ describe Solver do
       expect(@solver.factorial(0)).to eq 1
       expect(@solver.factorial(1)).to eq 1
     end
+
+    it 'should return 720 when the number 6' do
+      expect(@solver.factorial(6)).to eq 720
+    end
   end
 end
