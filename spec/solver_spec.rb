8354b750294e4a0b3c075763734f5514b5238512
Okechukwu-muokwugwo <okei.muokwugwo@gmail.com>
2023-01-05 13:53:46 +0100

    Create test for factorial n =0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../solver'
 
-describe Solver do
-  
+describe Solver 
+before do
+  @solver = Solver.new
+end
+
+  describe '#factorial' do
+    it 'should return the fatorial of N' do
+      expect(@solver.factorial(0)).to eq 1
+    end
+  end
 end
